825d9c26204838410b16e947c66124befda0738a
José Támara <jochitoel1@hotmail.com>
2018-02-23 05:53:24 -0500

    Add files via upload

--- src/components/nasa_list_item.js
+++ src/components/nasa_list_item.js
@@ -3,13 +3,13 @@ import './nasa_list_item.css'
 function NasaListItem ({nasa}){
 	return(
 		<div className="cardNasa">
-			<div className="titleNasa">
-				<h2>{nasa.title}</h2>
-			</div>
 			<div className="imageContent">
 				<a target="_blank" href={nasa.url}>
 					<img className="imageNasa" src={nasa.url} />
 				</a>
+			</div>
+			<div className="titleNasa">
+				<h2>{nasa.title}</h2>
 			</div>
 			<div className="dateNasa">
 				<p>{nasa.date}
